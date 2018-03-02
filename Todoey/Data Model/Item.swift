afc4316bdeb64ff6ea75793f2893cb88ac332885
John Yi <kyungwonyi@kyungwonui-macbook-pro.local>
2018-03-03 08:09:19 +0900

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by KYUNGWON YI on 2018. 3. 2..
+//  Copyright © 2018년 KYUNGWON YI. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
